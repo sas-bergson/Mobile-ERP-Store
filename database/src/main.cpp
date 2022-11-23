a2d27f275123bb3c11ceb9774b74d38c96aeb1c6
sas <sasbergson@gmail.com>
2022-11-23 01:57:17 +0100

    implementation for sqlite_database constructor, create and drop methods

---
+++ database/src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include  "sqlite_database.hpp"
+ 
+ int main(int argc, char **argv){
+     return 0;
+}
---
+++ database/src/sqlite_database.cpp
@@ -0,0 +1,58 @@
+#include <sqlite_database.hpp>
+#include <sqlite3.h>
+#include <sstream>
+#include <fstream>
+
+Sqlite_db::Sqlite_db(){
+    this->name = "test_db";
+    std::stringstream ss;
+    ss <<"file:"<< this->name;
+    this->uri= ss.str();
+    this->type = SQLite_db;
+}
+Sqlite_db::Sqlite_db(string name, string  uri, DatabaseType  type=SQLite_db){
+    this->name = name;
+    this->uri = uri;
+    this->type = type;
+}
+Sqlite_db::~Sqlite_db(){}
+
+string Sqlite_db::get_name(){return this->name;}
+string Sqlite_db::get_uri(){return this->uri;}
+DatabaseType Sqlite_db::get_type(){return this->type;}
+
+int Sqlite_db::create(){
+    sqlite3 *db;
+    int ret;
+    if (SQLITE_OK != (ret = sqlite3_initialize()))
+    {
+        fprintf(stderr, "Failed to initialize library: %d\n", ret);
+        return ret;
+    }
+   if(SQLITE_OK != (ret = sqlite3_open(uri.c_str(), &db))) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return ret;
+   } 
+   fprintf(stdout, "Open database successfull !!!!: %s\n", sqlite3_errmsg(db));
+   sqlite3_close(db);
+    return SQLITE_OK;
+}
+
+int Sqlite_db::drop(){
+    sqlite3 *db;
+    int ret;
+    if (SQLITE_OK != (ret = sqlite3_initialize()))
+    {
+        fprintf(stderr, "Failed to initialize library: %d\n", ret);
+        return ret;
+    }
+   if(SQLITE_OK != (ret = sqlite3_open(uri.c_str(), &db))) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return ret;
+   } 
+   fprintf(stdout, "Open database successfull !!!!: %s\n", sqlite3_errmsg(db));
+   sqlite3_close(db);
+    return SQLITE_OK;
+}
