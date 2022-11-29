2058c35c35e55969b5a7fdce871eaa3784adbaad
Fomo123K <118396428+Fomo123K@users.noreply.github.com>
2022-11-29 05:38:57 -0800

    Added Staff Class

---
+++ staff/Staff_Attendance_Management/include/staff.h
@@ -0,0 +1,21 @@
+#ifndef STAFF_H
+#define STAFF
+
+#include<iostream>
+#include <string.h>
+#include <sql>
+
+using namespace std;
+class staff{
+    public:
+        int id;
+        string name, email, address, tel;
+        void enrollStaf(int id, string nm, string email, string address, string telephone){
+
+            //Insert C++ SQLite commands to insert staff records into DB.
+
+        }
+}
+
+#endif //Close header file
+//KERNYUY FOMO
