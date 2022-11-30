1990933afe2766af38aa6fb47fe97b8e1f3157f9
Dark-1616 <118621737+Dark-1616@users.noreply.github.com>
2022-11-30 06:32:59 +0100

    Add files via upload

---
+++ maintenance/src/Register.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_pushButtonRegister_clicked()
+{
+ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+
---
+++ maintenance/src/Registerr.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+#include "..\include\Registerr.h"
+using namespace std;
+
+
+
+	int main(){
+
+		cout << "Type in your company's name \n" << endl;
+		cin>>CompanyName;
+
+
+		cout<<"Type in your company's address \n" <<endl;
+		cin>>CompanyAddress;
+
+
+		cout<<"Type in your company's contact number:+237 \n " <<endl;
+		cin>>CompanyContNumber;
+
+
+		cout<<"Type in your company's website(optional)\n " <<endl;
+		cin>>CompanyWebsite;
+
+		cout<<"Type in your company's email \n" <<endl;
+		cin>>CompanyEmail;
+
+		return 0;
+
+
+}
