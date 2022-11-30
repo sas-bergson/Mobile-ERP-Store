5db63eb8392ac2a0887a6db3975870210f8c4c4b
Dark-1616 <118621737+Dark-1616@users.noreply.github.com>
2022-11-30 06:34:00 +0100

    Add files via upload

---
+++ maintenance/Include/Registerr.h
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+
+	int main(){
+	   char CompanyName[18];
+
+		char CompanyAddress[50];
+
+		int CompanyContNumber[15];
+
+		char CompanyWebsite[50];
+
+		char CompanyEmail[30];
+
+		return 0;
+
+
+}
