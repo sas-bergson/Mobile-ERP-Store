919b224c96f7837b7e54123a76b91cc883012649
fomen2001 <118531985+fomen2001@users.noreply.github.com>
2022-12-05 22:42:35 +0100

    Update main.cpp

--- sales/src/main.cpp
+++ sales/src/main.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 #include "recetmoy.h"
 #include "attractivite.h"
+#include "Conversion.h"
 int main(){
-	cout<<"welcome to the indicator of perfomance of a store"<<endl;//debut du programma
+	cout<<"welcome to the indicator of perfomance of a store"<<endl;//debut du programme
+	
     Recetmoy p1(1);//classe Recetmoy
+    p1.store();
 	p1.affiche();
 	
-	Attractivite t1(0,0,0,0);//classe Attractivite
+	
+	Attractivite t1(0.0,0.0,0.0,0.0);//classe Attractivite
 	t1.infos();
 	t1.calcul();
 	
+	Conversion p(0.0,0.0);//classe conversion
+	p.affiche();
+	p.operation();
 
 	return 0;
 }
