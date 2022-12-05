76ca6ac312416dbecc3882c1ba59e4ec64cd8096
fomen2001 <118531985+fomen2001@users.noreply.github.com>
2022-12-05 22:38:32 +0100

    Add files via upload

---
+++ sales/bin/Conversion.h
@@ -0,0 +1,16 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+class Conversion
+{
+	public:
+		Conversion(double recu, double personne);
+		void affiche();
+		void operation();
+	private:
+		double recu;
+		double personne;
+		
+};
+
+#endif
