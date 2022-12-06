5a37be09e94977dd52c4fe19080e3f30b13d92b0
baimamboukar <baimamboukar@gmail.com>
2022-12-06 05:43:27 +0100

    init db class

---
+++ staff/include/database.h
@@ -0,0 +1,35 @@
+/*
+BAIMAM BOUKAR JEAN JACQUES
+email: baimam.jeanjacque@ictuniversiy.edu.cm
+tel: (+237) 690535759
+course: Introduction to mobile apps development
+instructor: Engr. Daniel Moune
+*/
+
+#include<string>
+#include<vector>
+using namespace std;
+
+// template class of the SQLite Database class
+class SQLiteDatabase{
+    /*----------DATA ATTRIBUTES--------------*/
+    private:
+        string name;
+    public:
+    /*----------CONSTRUCTORS--------------*/
+        SQLiteDatabase();
+
+
+    /*----------MODIFIERS(GETTERS & SETTERS)--------------*/
+
+
+    /*----------CLASS METHODS--------------*/
+        int open_databse(string name);
+        int create();
+        T fetch();
+        vector<T> fetchAll();
+
+
+     /*----------DESTRUCTORS--------------*/
+        ~SQLiteDatabase();
+}
